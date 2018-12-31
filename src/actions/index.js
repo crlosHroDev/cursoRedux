8fb7e1c10ab5f433c9846570e9e6ebb7dc19e72e
crlosHroDev <chernandezDev@gmail.com>
2018-12-31 11:55:31 -0500

    curso redux finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
+import {SEARCH_ENTITIES,OPEN_MODAL,CLOSE_MODAL,SEARCH_ASYNC_ENTITIES,IS_LOADING} from '../action-types/index'
 
 
 export function openModal(mediaId){
     return{
-        type:'OPEN_MODAL',
+        type:OPEN_MODAL,
         payload:{
             mediaId
         }
@@ -11,15 +12,34 @@ export function openModal(mediaId){
 
 export function closeModal(){
     return{
-        type:'CLOSE_MODAL',
+        type:CLOSE_MODAL,
         }
     }
 
 export function searchEntities(query){
     return{
-        type:'SEARCH_ENTITIES',
+        type:SEARCH_ENTITIES,
         payload:{
             query
         }
     }
+}
+
+export function isLoading(value){
+    return{
+        type:IS_LOADING,
+        payload:{
+            value
+        }
+    }
+}
+export function searchAsyncEntities(query){
+    return (dispatch)=>{
+        dispatch(isLoading(true))
+        setTimeout(()=>{
+            dispatch(isLoading(false))
+            dispatch(searchEntities(query))
+        },5000)
+    }
+    
 }
--- src/entries/home.js
+++ src/entries/home.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import {Map as map} from 'immutable';
+import logger from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import reducer from '../reducers/index';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 // import data from '../api.json';
 import data from '../schemas/index';
-// console.log('Hola mundo!' )
 
+// function logger({getState,dispatch}){
+//     return (next)=>{
+//         return (action)=>{
+//             console.log('vamos a enviar esta accion',action)
+//             const value=next(action)
+//             console.log('este es mi nuevo estado',getState().toJS())
+//             return value
+//         }
+//     } 
+// }
 
-// const initialState={
-//     data:{
-//         // ...data,
-//         entities:data.entities,
-//         categories:data.result.categories,
-//         search:[]
-//     },
-//     modal:{
-//         visibility:false,
-//         mediaId:null
-//     }
+// const logger=({getState,dispatch})=>next=>action=>{
+//     console.log('vamos a enviar esta accion',action)
+//     const value=next(action)
+//     console.log('este es mi nuevo estado',getState().toJS())
+//     return value
 // }
 const store =createStore(
     reducer,
     map(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+    applyMiddleware(
+        logger,
+        thunk
+        )
+    )
 )
 
 console.log(store.getState())
